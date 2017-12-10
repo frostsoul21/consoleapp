b20c8e70278d3808b4aada558a945690fa749f3a
frostsoul21 <34131272+frostsoul21@users.noreply.github.com>
2017-12-09 20:24:57 -0500

    Update Program.cs

--- jasonExercise/Program.cs
+++ jasonExercise/Program.cs
@@ -10,6 +10,24 @@ namespace jasonExercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please type a number");
+            int result = int.Parse(Console.ReadLine());
+            int resultPlus1 = result + 1;
+            Console.WriteLine("Your new number is:");
+            Console.WriteLine(resultPlus1);
+                if (resultPlus1 % 3 == 0)
+                {
+                Console.WriteLine("fizz");
+                }
+                else if (resultPlus1 % 5 == 0)
+                {
+                Console.WriteLine("buzz");
+                }
+                else
+                {
+                Console.WriteLine("The new number is not evenly devisible by 3 or 5. ");
+                }
+            Console.ReadLine();
         }
     }
 }
